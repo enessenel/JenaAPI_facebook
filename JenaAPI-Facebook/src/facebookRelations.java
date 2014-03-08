d99301dd68caf9090464c6f0fa90825f17e6dbff
enessenel <enessenel@outlook.com>
2014-03-09 00:44:30 +0200

    a lot of classes and relations included. creates a sample case and prints as rdf.

--- JenaAPI-Facebook/src/facebookRelations.java
+++ JenaAPI-Facebook/src/facebookRelations.java
@@ -1,6 +1,19 @@
+
+
+
+/* Author: Enes Þenel - S003272
+ * 
+ * facebookRelations class for defining some objects, subjects and predicates,
+ * that can define a simple facebook relationship system.
+ * 
+ * I defined first classes and predicates and then created them for simplicity and modularity.
+ */
+
+
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.ontology.DatatypeProperty;
 import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.MinCardinalityRestriction;
 import com.hp.hpl.jena.ontology.ObjectProperty;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
@@ -13,6 +26,7 @@ import com.hp.hpl.jena.vocabulary.XSD;
 
 public class facebookRelations {
 
+
 	public OntClass person;
 	public OntClass malePerson;
 	public OntClass femalePerson;
@@ -24,7 +38,10 @@ public class facebookRelations {
 	public OntClass video;
 	public OntClass picture;
 	public OntClass department;
-	
+	public OntClass city;
+	public OntClass animal;
+	public OntClass dog;
+
 	public DatatypeProperty hasAge;
 	public DatatypeProperty hasBirthday;
 	public DatatypeProperty graduatedIn;
@@ -45,11 +62,19 @@ public class facebookRelations {
 	public ObjectProperty hasDepartment;
 	public ObjectProperty hasGraduated;
 	public ObjectProperty inSchool;
-	
+	public ObjectProperty bornIn;
+	public ObjectProperty livesIn;
+	public ObjectProperty hasPet;
+	public ObjectProperty liked;
+	public ObjectProperty hasDog;
+
+	MinCardinalityRestriction hasAgeMinCard;
+	MinCardinalityRestriction hasDepartmentMinCard;
+
 	public OntModel ontModel;
 	public String ns;
 	public String baseURI;
-	
+
 
 	public facebookRelations(){
 
@@ -57,12 +82,14 @@ public class facebookRelations {
 		ns = new String("http://www.facebook.com/onto1#");
 		baseURI = new String("http://www.facebook.com/onto1");
 		Ontology onto = ontModel.createOntology(baseURI);	
+
 		initializeClasses();
 		intializeDatatypeProperties();
 		initializeObjectProperties();
 		setSubclasses();
 		setDisjoints();
 		setDomainAndRange();
+		setCardinalityRestrictions();
 	}
 
 
@@ -79,6 +106,9 @@ public class facebookRelations {
 		media 			 = ontModel.createClass(ns + "Media");
 		video 			 = ontModel.createClass(ns + "Video");
 		picture			 = ontModel.createClass(ns + "Picture");
+		city			 = ontModel.createClass(ns + "City");
+		animal			 = ontModel.createClass(ns + "Animal");
+		dog 			 = ontModel.createClass(ns + "Dog");
 	}
 
 	public void intializeDatatypeProperties(){
@@ -105,6 +135,11 @@ public class facebookRelations {
 		inPicture 		 = ontModel.createObjectProperty(ns + "inPicture");
 		hasDepartment    = ontModel.createObjectProperty(ns + "hasDepartment");
 		hasGraduated     = ontModel.createObjectProperty(ns + "hasGraduated");
+		bornIn  		 = ontModel.createObjectProperty(ns + "bornIn");
+		livesIn 		 = ontModel.createObjectProperty(ns + "livesIn");
+		hasPet 			 = ontModel.createObjectProperty(ns + "hasPet");
+		liked 			 = ontModel.createObjectProperty(ns + "liked");
+		hasDog 			 = ontModel.createObjectProperty(ns + "hasDog");
 	}
 
 	public void setSubclasses(){
@@ -116,15 +151,17 @@ public class facebookRelations {
 		school.addSubClass(university);
 		media.addSubClass(video);
 		media.addSubClass(picture);
+		animal.addSubClass(dog);
 
 		hasParent.addSubProperty(hasFather);
 		hasParent.addSubProperty(hasMother);
 		hasSibling.addSubProperty(hasSister);
 		hasSibling.addSubProperty(hasBrother);
 		hasMedia.addSubProperty(hasVideo);
-		hasMedia.addSubProperty(inVideo);
+		inMedia.addSubProperty(inVideo);
 		hasMedia.addSubProperty(hasPicture);
-		hasMedia.addSubProperty(inPicture);
+		inMedia.addSubProperty(inPicture);
+		hasPet.addSubProperty(hasDog);
 	}
 
 	public void setDisjoints(){
@@ -141,22 +178,24 @@ public class facebookRelations {
 
 		video.addDisjointWith(picture);
 		picture.addDisjointWith(video);
-		
-		
 	}
 
 	public void setDomainAndRange(){
 
+		liked.setDomain(person);
+		liked.setDomain(animal);
+		liked.setRange(media);
+
 		hasGraduated.setDomain(person);
 		hasGraduated.setRange(department);
-		
+
 		hasAge.setDomain(person);
 		hasAge.setDomain(school);
 		hasAge.setRange(XSD.integer);
 
 		graduatedIn.setDomain(person);
 		graduatedIn.setRange(XSD.date);
-		
+
 		hasBirthday.setDomain(person);
 		hasBirthday.setRange(XSD.date);
 
@@ -166,7 +205,32 @@ public class facebookRelations {
 		hasParent.setDomain(person);
 		hasParent.setRange(person);
 
+		hasFriend.setDomain(person);
+		hasFriend.setDomain(person);
 		hasDepartment.setDomain(university);
 		hasDepartment.setRange(department);
+
+		bornIn.setDomain(person);
+		bornIn.setRange(city);
+
+		livesIn.setDomain(person);
+		livesIn.setRange(city);
+
+		hasMedia.setDomain(animal);
+		hasMedia.setDomain(person);
+		hasMedia.setRange(media);
+
+		inMedia.setDomain(animal);
+		inMedia.setDomain(person);
+		inMedia.setRange(media);
+
+		hasPet.setDomain(person);
+		hasPet.setRange(animal);
+	}
+
+	public void setCardinalityRestrictions(){
+
+		hasAgeMinCard = ontModel.createMinCardinalityRestriction(null, hasAge, 1);
+		hasDepartmentMinCard = ontModel.createMinCardinalityRestriction(null, hasDepartment, 1);
 	}
 }
--- JenaAPI-Facebook/src/hw1.java
+++ JenaAPI-Facebook/src/hw1.java
@@ -1,47 +1,159 @@
+/*
+ * 	Author: Enes Þenel - S003272
+ * 
+ * Includes main method for saving rdf/xml format and also create Individuals to
+ * create sample data. 
+ * 
+ */
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Statement;
 
-
 public class hw1 {
-public static facebookRelations fb;
+
+	public static facebookRelations fb;
+
 	public static void main(String[] args) {
+
 		fb = new facebookRelations();
-		
-		createJohn();
+
+		Individual computerScience = fb.department.createIndividual(fb.ns
+				+ "ComputerScience");
+		Individual stanford = fb.university.createIndividual(fb.ns
+				+ "StanfordUniversity");
+		createDepartment(stanford, computerScience);
+
+		// male with birthdate 31.07.1980
+		Individual john = fb.malePerson.createIndividual(fb.ns + "John");
+		Literal birthdayOfJohn = fb.ontModel.createTypedLiteral("31.07.1980",
+				XSDDatatype.XSDdate);
+		createBirthdayRelation(john, birthdayOfJohn);
+
+		// graduated from CS at Stanford in 2004
+		Literal graduation = fb.ontModel.createTypedLiteral("2004",
+				XSDDatatype.XSDdate);
+		setGraduation(john, computerScience, graduation);
+
+		// Person called jane with 16.06.1970 birthdate
+		Individual jane = fb.femalePerson.createIndividual(fb.ns + "Jane");
+		Literal birthdayOfJane = fb.ontModel.createTypedLiteral("31.07.1980",
+				XSDDatatype.XSDdate);
+		createBirthdayRelation(jane, birthdayOfJane);
+
+		// she is from NYC but lives in istanbul
+		Individual nyc = fb.city.createIndividual(fb.ns + "NYC");
+		setBornedIn(jane, nyc);
+		Individual istanbul = fb.city.createIndividual(fb.ns + "Istanbul");
+		livesIn(jane, istanbul);
+
+		// john has a pic with jane in his profile
+		Individual picture1 = fb.picture.createIndividual(fb.ns + "Picture1");
+		hasPicture(john, picture1);
+		inPicture(jane, picture1);
+		inPicture(john, picture1);
+
+		// wendy is a dog with fb account has a video in jane's profile
+		Individual wendy = fb.dog.createIndividual(fb.ns + "Wendy");
+		Individual dogVideo = fb.video.createIndividual(fb.ns + "dogVid");
+		inVideo(wendy, dogVideo);
+		inVideo(jane, dogVideo);
+		hasVideo(jane, dogVideo);
+
+		// john and jane likes wendy's pic
+		Individual profPic = fb.picture.createIndividual(fb.ns + "profPic");
+		hasPicture(wendy, profPic);
+		likesMedia(john, profPic);
+		likesMedia(jane, profPic);
+
+		// jane and john are friends
+		setFriends(jane, john);
+		setFriends(john, jane);
+
 		try {
 
-			  File file= new File("data.rdf");
-			 fb.ontModel.write(new FileOutputStream(file));
+			File file = new File("data.rdf");
+			fb.ontModel.write(new FileOutputStream(file));
 
-			} catch (IOException e) {
-			  e.printStackTrace();
-			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	
-	public static void createJohn(){
-		Individual computerScience = fb.department.createIndividual(fb.ns + "ComputerScience");
-		Individual stanford = fb.university.createIndividual(fb.ns + "StanfordUniversity");
-		Statement department = fb.ontModel.createStatement(stanford, fb.hasDepartment,computerScience );
+	public static void setFriends(Individual i1, Individual i2) {
+
+		Statement friends = fb.ontModel.createStatement(i1, fb.hasFriend, i2);
+		fb.ontModel.add(friends);
+	}
+
+	public static void likesMedia(Individual i1, Individual i2) {
+
+		Statement likes = fb.ontModel.createStatement(i1, fb.liked, i2);
+		fb.ontModel.add(likes);
+	}
+
+	public static void inPicture(Individual i1, Individual i2) {
+
+		Statement picture = fb.ontModel.createStatement(i1, fb.inPicture, i2);
+		fb.ontModel.add(picture);
+	}
+
+	public static void hasPicture(Individual i1, Individual i2) {
+
+		Statement picture = fb.ontModel.createStatement(i1, fb.hasPicture, i2);
+		fb.ontModel.add(picture);
+	}
+
+	public static void inVideo(Individual i1, Individual i2) {
+
+		Statement video = fb.ontModel.createStatement(i1, fb.inVideo, i2);
+		fb.ontModel.add(video);
+	}
+
+	public static void hasVideo(Individual i1, Individual i2) {
+
+		Statement video = fb.ontModel.createStatement(i1, fb.hasVideo, i2);
+		fb.ontModel.add(video);
+	}
+
+	public static void createDepartment(Individual i1, Individual i2) {
+
+		Statement department = fb.ontModel.createStatement(i1,
+				fb.hasDepartment, i2);
 		fb.ontModel.add(department);
-		
-		Individual john = fb.malePerson.createIndividual(fb.ns + "John");
-		Literal birthday = fb.ontModel.createTypedLiteral("31.07.1980", XSDDatatype.XSDdate);
-		Statement birthdayOfJohn = fb.ontModel.createStatement(john, fb.hasBirthday, birthday);
-		fb.ontModel.add(birthdayOfJohn);
-			
-		Statement graduatedFrom = fb.ontModel.createStatement(john, fb.hasGraduated, computerScience);
-		fb.ontModel.add(graduatedFrom);
-		Literal graduation = fb.ontModel.createTypedLiteral("2004", XSDDatatype.XSDdate);
-		Statement graduatedIn = fb.ontModel.createStatement(john, fb.graduatedIn, graduation);
-		fb.ontModel.add(graduatedIn);
-		
 	}
+
+	public static void createBirthdayRelation(Individual i1, Literal l1) {
+
+		Statement birthday = fb.ontModel
+				.createStatement(i1, fb.hasBirthday, l1);
+		fb.ontModel.add(birthday);
+	}
+
+	public static void setGraduation(Individual i1, Individual i2, Literal g1) {
+
+		Statement graduatedFrom = fb.ontModel.createStatement(i1,
+				fb.hasGraduated, i2);
+		fb.ontModel.add(graduatedFrom);
+		Statement graduatedIn = fb.ontModel.createStatement(i1, fb.graduatedIn,
+				g1);
+		fb.ontModel.add(graduatedIn);
+	}
+
+	public static void setBornedIn(Individual i1, Individual i2) {
+
+		Statement bornedIn = fb.ontModel.createStatement(i1, fb.bornIn, i2);
+		fb.ontModel.add(bornedIn);
+	}
+
+	public static void livesIn(Individual i1, Individual i2) {
+
+		Statement livesIn = fb.ontModel.createStatement(i1, fb.livesIn, i2);
+		fb.ontModel.add(livesIn);
+	}
+
 }
